1bcba2bec1c8b7dfe17f50758e9a8c4b621cce53
Rafael Herrera <rafaelherrera731@gmail.com>
2022-03-26 15:31:08 -0700

    grounwork/layout of the JS, todo list

---
+++ README.md
@@ -0,0 +1,6 @@
+To DOs
+CSS 
+Bootstrap col to be like the example
+Functions in JS to save text, update highlighted time
+Save buttons
+moment to match the rows
--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // var moment = moment().format("H")
 
-//current day variable
-//var currentDay = $("currentDay")
+//gives the current month,day,year,hours,minutes,seconds and AM/PM
+var today = moment();
+$("#day").text(today.format("MMMM Do YYYY, h:mm:ss a"));
 
-$("currentDay").text(now.format("dddd MMMM DD, YYYY"));
+
+//looks up the current time for the highligted hour
+var currentTime = {text: moment().format("h:00 A"), hour: moment().hour()};
+
+
+var store = window.localStorage;
+
+var now = moment(); 
+
+//funtion to highlight current hour
+function(){
+
+}
+
+//function to save text in text box
+
+//make a functional button to save
+

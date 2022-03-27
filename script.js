4836ba945b47b5646b29e9f52a290f30041767cf
Rafael Herrera <rafaelherrera731@gmail.com>
2022-03-27 14:31:30 -0700

    some JS tinkering

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $("#day").text(today.format("MMMM Do YYYY, h:mm:ss a"));
 
 //looks up the current time for the highligted hour
 var currentTime = {text: moment().format("h:00 A"), hour: moment().hour()};
-
+console.log(currentTime)
 
 var store = window.localStorage;
 
@@ -15,20 +15,20 @@ var now = moment();
 
 var row = $("#row")
 
-var currentHour = $("#currentHour")
+var currentHour = $(".currentHour")
 
-var textBox = $("#textBox")
+var textBox = $(".textBox")
 
-var saveButton = $("#SaveButton")
+var saveButton = $(".saveButton")
 
-//funtion to highlight current hour
-// function color(time){
-//   return time.text === currentHour.text
-//     ? "bg-danger"
-//     : time.hour < now
-//     ? "bg-secondary"
-//     : "bg-success";
-// }
+//funtion to highlight current hour, in progess
+function color(time){
+  return time.hour === row.text
+    ? "bg-danger"
+    : time.hour < now
+    ? "bg-secondary"
+    : "bg-success";
+}
 
 //function to save text in text box
 //make a functional button to save
@@ -37,3 +37,18 @@ saveButton.on("click", function(){
   var text = $(this).siblings(textBox).val();
   var time = $(this).parent().attr("id")
 })
+//another method I could try
+
+// if (localStorage["textBox"])
+// {
+//   textBox = localStorage["textBox"];
+//   document.getElementById("textBox").value = textBox;
+
+// }
+
+// saveButton.on("click", function()
+// {
+//    textBox = document.getElementById("textBox").value ;
+//         //localStorage["user"] = user ;
+//         localStorage.setItem("textBox", textBox) ;
+// })

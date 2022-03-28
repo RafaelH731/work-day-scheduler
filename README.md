ed689ba6058afb3a112ebfc50f64310fdee87d86
Rafael Herrera <rafaelherrera731@gmail.com>
2022-03-27 22:57:44 -0700

    updates on the README and JS file

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-To DOs
-CSS 
-Bootstrap col to be like the example
-Functions in JS to save text, update highlighted time
-Save buttons
-moment to match the rows
+Description
+In this application we are using moment.js to make a day planner. The planner will be able to keep track of task that are written down on the website. It will highlight the current hour and will be highlighted. Anything past the hour will be highlighed a different hour.
+
+
+Table of Contents
+HTML code uses bootstrap to make the rows that consist of the time, textbox and a save button, and uses col to arrange the colums.
+JS code uses moment.js to keep track of time
+Use JQuery to get familiar with it just in case we run into it in the future
+
+![Screenshot of Applicationm]()
+
+[Work Day Generator](https://rafaelh731.github.io/work-day-scheduler/)
--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $("#day").text(today.format("MMMM Do YYYY, h:mm:ss a"));
 var currentTime = {text: moment().format("h:00 A"), hour: moment().hour()};
 console.log(currentTime)
 
-var store = window.localStorage;
+//var store = window.localStorage;
 
 var now = moment(); 
 
@@ -30,13 +30,26 @@ function color(time){
     : "bg-success";
 }
 
+//another way i am trying
+
+  if (currentHour == now) {
+    $("row").css({ "background-color": "#5E5B52"
+
+    })
+  }
+  
+
 //function to save text in text box
 //make a functional button to save
-saveButton.on("click", function(){
-  console.log(this);
-  var text = $(this).siblings(textBox).val();
-  var time = $(this).parent().attr("id")
-})
+ $(".saveButton").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".textBox").val(); 
+        var time = $(this).parent().attr("id"); 
+
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
 //another method I could try
 
 // if (localStorage["textBox"])
